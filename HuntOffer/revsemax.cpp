cc8cd2c79ecba527226585012d65ee361b54376f
robin <chou_robin@hotmail.com>
2016-07-28 13:12:52 +0800

    add

---
+++ HuntOffer/revsemax.cpp
@@ -0,0 +1,51 @@
+/*************************************************************************
+	> File Name: revsemax.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 2016年07月28日 星期四 10时48分19秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+int revsemax(int *num, int len)
+{
+    if(num == NULL || len <= 0)
+        return 0;
+    int *tmparr = new int[len];
+    for(int i=0; i<len; ++i)
+        tmparr[i] = 1;
+
+    int j = 0;
+    int maxnum = 1;
+    for(int i=1;i<len;++i)
+    {
+        j = i-1;
+        maxnum = 1;
+        while(j>=0)
+        {
+            if(num[i] < num[j] && maxnum <= tmparr[j])
+                maxnum = tmparr[j] + 1;
+            j--;
+        }
+        tmparr[i] = maxnum;
+    }
+
+    //找到最大的数字
+    maxnum = 1;
+    for(int i=1; i<len; ++i)
+    {
+        if(tmparr[i] > maxnum)
+            maxnum = tmparr[i];
+    }
+    delete[] tmparr;
+    return maxnum;
+}
+
+int main()
+{
+    int arr[8] = {1,-2,3,10,-4,7,2,-5};
+    cout << "revse: " << revsemax(arr, 8) << endl;
+    return 0;
+}
+
